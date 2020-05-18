01e6b66938bfb4ab4e0d582936ab7ebfdec7ba26
Gustavo Antoniassi <gus.antoniassi@gmail.com>
2020-05-18 20:09:59 -0300

    Add SSM studies

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Studies
+
+Studies and resources on AWS Services 
---
+++ ssm/ec2-poc-cfn/update-stack.sh
@@ -0,0 +1,16 @@
+STACK_NAME="$1"
+TEMPLATE_FILE=${2:-main.yaml}
+PARAMETER_FILE=${3:-parameters.json}
+AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}
+STACK_COMMAND="create-stack"
+
+if aws cloudformation describe-stacks --region=$AWS_DEFAULT_REGION --stack-name=$STACK_NAME >/dev/null 2>&1; then
+    STACK_COMMAND="update-stack"
+fi
+
+aws cloudformation $STACK_COMMAND \
+--stack-name $STACK_NAME \
+--template-body file://./$TEMPLATE_FILE \
+--parameters file://./$PARAMETER_FILE \
+--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+--region=$AWS_DEFAULT_REGION
